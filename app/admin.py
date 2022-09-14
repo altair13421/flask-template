3008ddad86f61bdc6f59ca08954dc7ae2b947d84
yeager <faezakhtar110@outlook.com>
2022-09-14 23:02:39 +0500

    fixing current pages

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,11 @@
 from app import app, db, ma
 import random
-from flask import render_template, redirect, url_for, current_app
+from flask import render_template, redirect, url_for, current_app, flash, request
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField, SubmitField, EmailField
+from werkzeug.urls import url_parse
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 
 bootstrap = Bootstrap(app)
@@ -18,6 +19,10 @@ class Admins(db.Model):
     admin_username = db.Column(db.String, unique=True)
     admin_password = db.Column(db.String)
     admin_token = db.Column(db.String)
+    
+    @login.user_loader
+    def load_user(id):
+        return Admins.query.get(int(id))
     
     def change_username(self, username):
         self.admin_username = username
@@ -45,7 +50,7 @@ class Admin_schema(ma.Schema):
         fields = ("username", "password")
 
 # FORMS
-class Admin_Login(FlaskForm):
+class Admin_Login_Form(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Enter the Panel")
@@ -54,9 +59,34 @@ class Admin_Login(FlaskForm):
 
 @app.route('/admin/login', methods=["GET", "POST"])
 def admin_login():
-    return render_template('admin_login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('main_page'))
+    form = Admin_Login_Form()
+    if form.validate_on_submit():
+        user = Admins.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or Password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main_page')
+        return redirect(next_page)
+    return render_template(
+        'admin_login.html', 
+        title="Login",
+        name_form='Admin Login',
+        form = form,
+    )
 
-@login.login_required
+@login_required
 @app.route("/admin/home", methods=["GET"])
 def admin_home():
     pass
+
+
+@app.route('/admin/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('admin_login'))
--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 class Configs(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "my-secret-key-yayyy"
