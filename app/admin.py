3a9e1f338e9e64faf3636ac5d19f3b229b4b7f79
yeager <faezakhtar110@outlook.com>
2022-09-15 00:07:31 +0500

    ignore file added

--- app/admin.py
+++ app/admin.py
@@ -2,20 +2,28 @@ from app import app, db, ma
 import random
 from flask import render_template, redirect, url_for, current_app, flash, request
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager, login_required, login_user, current_user, logout_user
+from flask_login import LoginManager, login_required, login_user, current_user, logout_user, UserMixin
 from flask_wtf import FlaskForm
 from werkzeug.urls import url_parse
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 
 bootstrap = Bootstrap(app)
 login = LoginManager(app=app)
 login.login_view = 'admin_login'
+login_messages = (
+    "You must Be logged in",
+    "Are you sure you're Logged in",
+    "Drunks should Log in",
+    "Login or Gimme your Soul",
+    "Soul accepted, now You may Login",
+    "BRING ME THEIR SOULS, or just login",
+)
 
 # Admin Model
-class Admins(db.Model):
+class Admins(db.Model, UserMixin):
     __tablename__ = "admin"
-    admin_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     admin_username = db.Column(db.String, unique=True)
     admin_password = db.Column(db.String)
     admin_token = db.Column(db.String)
@@ -29,10 +37,13 @@ class Admins(db.Model):
     
     def change_password(self, password):
         self.admin_password = password
-        
+    
+    def check_password(self, password):
+        return self.admin_password == password
+    
     def get_token(self):
         return self.admin_token
-    
+        
     def set_token(self):
         random_string = ''
         for _ in range(16):
@@ -56,18 +67,19 @@ class Admin_Login_Form(FlaskForm):
     submit = SubmitField("Enter the Panel")
     pass
 
-
+# THE SHITTER
 @app.route('/admin/login', methods=["GET", "POST"])
 def admin_login():
     if current_user.is_authenticated:
-        return redirect(url_for('main_page'))
+        return redirect(url_for('admin_home'))
     form = Admin_Login_Form()
     if form.validate_on_submit():
-        user = Admins.query.filter_by(username=form.username.data).first()
+        user = Admins.query.filter_by(admin_username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid Username or Password')
             return redirect(url_for('admin_login'))
-        login_user(user, remember=form.remember_me.data)
+        login_user(user)
+        flash("Login Successful")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('admin_home')
@@ -79,10 +91,10 @@ def admin_login():
         form = form,
     )
 
-@login_required
 @app.route("/admin/home", methods=["GET"])
+@login_required
 def admin_home():
-    pass
+    return render_template("admin.html")
 
 
 @app.route('/admin/logout', methods=['GET', 'POST'])
@@ -90,3 +102,7 @@ def admin_home():
 def logout():
     logout_user()
     return redirect(url_for('admin_login'))
+
+@app.before_request
+def before_request():
+    login.login_message = login_messages[random.randint(0, len(login_messages)-1)]
